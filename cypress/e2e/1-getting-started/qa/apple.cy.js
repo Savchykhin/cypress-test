66909f20a8394e4ba0274c07b90f4a09e1a94d44
OLeksandr <savchykhin@gmail.com>
2023-02-04 13:50:59 -0800

    new apple trst

---
+++ cypress/e2e/1-getting-started/qa/apple.cy.js
@@ -0,0 +1,14 @@
+describe('Apple', function() {
+    it('Visits the homepage and checks the title', function() {
+      cy.visit('https://apple.com/')
+       cy.title().should('include', 'Apple')
+    })
+     it('Navigates to the MacBook Pro page and checks the product name', function() {
+      cy.visit('https://apple.com/')
+       cy.get('.ac-gn-link[href="/mac/"]').click()
+      cy.get('.chapternav-item-macbook-pro > .chapternav-link > .chapternav-icon').click()
+       cy.url().should('include', '/macbook-pro/')
+       cy.contains('MacBook Pro')
+    })
+  })
+ 
