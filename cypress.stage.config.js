8011430357f2e57466aec37acecb86687cc1ec11
OLeksandr <savchykhin@gmail.com>
2023-06-09 22:17:33 -0700

    test

--- cypress.stage.config.js
+++ cypress.stage.config.js
@@ -1,7 +1,17 @@
 const { defineConfig } = require("cypress");
+const defaultConfig = require("./cypress.config.js");
+const defu = require('defu');
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://staging-url.com',
-  },
-});
+module.exports = defineConfig(
+    defu(
+        {
+            e2e: {
+                baseUrl: 'http://stage-url.com',
+            },
+        },
+        defaultConfig
+    )
+);
+
+
+
