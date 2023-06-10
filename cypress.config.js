d64eb55d3a918c34a0e6a50c8164ef74f5286f29
OLeksandr <savchykhin@gmail.com>
2023-06-09 21:42:48 -0700

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    inlineAssets: true,
+  },
   e2e: {
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
