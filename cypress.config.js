1592dc1d904d720911f36309623630528e0ebeb0
OLeksandr <savchykhin@gmail.com>
2023-06-09 22:38:55 -0700

    test

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,22 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+
+      const environmentName = config.env.environmentName || 'prod'
+      const environmentFilename = `./${environmentName}.settings.json`
+      console.log('loading %s', environmentFilename)
+      const settings = require(environmentFilename)
+      if (settings.baseUrl) {
+        config.baseUrl = settings.baseUrl
+      }
+      if (settings.env) {
+        config.env = {
+          ...config.env,
+          ...settings.env,
+        }
+      }
+      console.log('loaded settings for environment %s', environmentName)
+      return config
     },
   },
 });
